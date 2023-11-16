1350178cc51f4721de2676b0485bc008309b9764
Ana Ceaicovschi <anna.ceacovschi@carasent.com>
2023-11-16 21:02:03 +0200

    COnfigure components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MovieListComponent} from "./components/movie-list-component/movie-list-component.component";
+import {HttpClientModule} from "@angular/common/http";
+import {NavBarComponent} from "./components/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, MovieListComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {MovieDetailComponent} from "./components/movie-detail/movie-detail.component";
+import {MovieListComponent} from "./components/movie-list-component/movie-list-component.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: MovieListComponent},
+  {path: 'details/:id', component: MovieDetailComponent},
+];
