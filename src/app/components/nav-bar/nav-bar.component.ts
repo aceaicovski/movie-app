fee2694923ee5f80fbed441eee62ac8e4fc81b0c
Ana Ceaicovschi <anna.ceacovschi@carasent.com>
2023-11-17 13:44:37 +0200

    Routing fix

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import {MatInputModule} from "@angular/material/input";
 import {FormsModule} from "@angular/forms";
 import {MovieService} from "../../services/movie.service";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-nav-bar',
@@ -16,10 +16,16 @@ export class NavBarComponent {
   public inputValue: string = '';
   @Output() searchValueChanged: EventEmitter<string> = new EventEmitter<string>();
 
-  constructor(private movieService: MovieService) {
+  constructor(private router: Router, private movieService: MovieService) {
   }
 
   public searchFilm(): void {
+    const currentPathname = this.router.url;
+
+    if (currentPathname !== "/") {
+      this.router.navigate([`/`]);
+    }
+
     this.movieService.setSearchMovieValue(this.inputValue);
   }
 }
